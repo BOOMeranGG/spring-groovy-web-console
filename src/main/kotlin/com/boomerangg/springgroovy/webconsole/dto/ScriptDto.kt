aca7b54c496fe5fc006ad14ab28c3fd9ab9e46a3
Achim Oppelt <achim.oppelt@mgm-tp.com>
2025-09-17 13:12:21 -0400

    Fix for JSON deserialization issue

--- src/main/kotlin/com/boomerangg/springgroovy/webconsole/dto/ScriptDto.kt
+++ src/main/kotlin/com/boomerangg/springgroovy/webconsole/dto/ScriptDto.kt
@@ -1,5 +1,8 @@
 package com.boomerangg.springgroovy.webconsole.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class ScriptDto(
+    @param:JsonProperty("script")
     val script: String,
 )
